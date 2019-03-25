952138a02303be33e4eae2e2d3941b5e098b0513
Doug Beasley <beasley.doug@gmail.com>
2019-03-25 14:36:59 -0700

    updating toolbar and menu

--- src/app/app-material/app-material.module.ts
+++ src/app/app-material/app-material.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatSliderModule, MatInputModule, MatCardModule, MatToolbarModule } from '@angular/material';
+import { MatSliderModule, MatInputModule, MatCardModule, MatToolbarModule, MatListModule, MatMenuModule, MatIconModule } from '@angular/material';
+
 
 @NgModule({
   declarations: [],
   imports: [CommonModule,
-    MatSliderModule, MatInputModule, MatCardModule, MatToolbarModule],
-  exports: [MatSliderModule, MatInputModule, MatCardModule, MatToolbarModule]
+    MatSliderModule, MatInputModule, MatCardModule, MatToolbarModule, MatListModule, MatMenuModule, MatIconModule],
+  exports: [MatSliderModule, MatInputModule, MatCardModule, MatToolbarModule, MatListModule, MatMenuModule, MatIconModule]
 })
 export class AppMaterialModule { }

195b39c31264c3f18fec8361ea242006a5ba73d6
Doug Beasley <beasley.doug@gmail.com>
2019-03-25 13:53:15 -0700

    renaming project

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to worms!');
+    expect(page.getTitleText()).toEqual('Welcome to spacerocks-ui!');
   });
 
   afterEach(async () => {
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'worms'`, () => {
+  it(`should have as title 'spacerocks-ui'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('worms');
+    expect(app.title).toEqual('spacerocks-ui');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to worms!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to spacerocks-ui!');
   });
 });

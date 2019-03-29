2edbf3789804cd677c8765fc8f7167565a109678
Doug Beasley <beasley.doug@gmail.com>
2019-03-29 15:28:24 -0700

    fixing up the ui

--- src/app/app-material/app-material.module.ts
+++ src/app/app-material/app-material.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatSliderModule, MatInputModule, MatCardModule, MatToolbarModule, MatListModule, MatMenuModule, MatIconModule } from '@angular/material';
-
+import { MatSliderModule,
+  MatInputModule,
+  MatCardModule,
+  MatToolbarModule,
+  MatListModule,
+  MatMenuModule,
+  MatIconModule,
+  MatExpansionModule } from '@angular/material';
 
 @NgModule({
   declarations: [],
   imports: [CommonModule,
-    MatSliderModule, MatInputModule, MatCardModule, MatToolbarModule, MatListModule, MatMenuModule, MatIconModule],
-  exports: [MatSliderModule, MatInputModule, MatCardModule, MatToolbarModule, MatListModule, MatMenuModule, MatIconModule]
+    MatSliderModule, MatInputModule, MatCardModule, MatToolbarModule, MatListModule, MatMenuModule, MatIconModule, MatExpansionModule],
+  exports: [MatSliderModule, MatInputModule, MatCardModule, MatToolbarModule, MatListModule, MatMenuModule, MatIconModule, MatExpansionModule]
 })
 export class AppMaterialModule { }
